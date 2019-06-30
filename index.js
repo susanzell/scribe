5054f4be8338c75ad55ed2f3b3dfc07b04b9d5f1
susanzell <susan.zell@gmail.com>
2019-06-30 10:28:54 -0700

    3 files modified, 1 added

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { render } from 'react-dom';
 import Hello from './Hello';
 import './style.css';
+import axios from 'axios';
 
 class App extends Component {
   constructor() {
